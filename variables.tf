3ea395036d659e28c3c4533f0fc5bfd1b3aad6c0
ChicomeXochitl <juan.m.garcia.mancera@gmail.com>
2026-04-09 11:34:57 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "launch-wizard-2" 
+    default     = "jenkins-sg-2026" 
 }
 
 variable "tag_name" { 
