9a36c19950ed01ab52ad09db96655588a1dfa1a8
ChicomeXochitl <juan.m.garcia.mancera@gmail.com>
2026-04-09 09:33:22 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    // bucket = "my-s3-bucket-for-tf-state"
     bucket = "juan-dev-tf-state-bucket"
-    //key = "main"
+    key = "main"
     region = "us-west-2"
-    //dynamodb_table = "my-dynamo-db-table-tf"
     dynamodb_table = "my-dynamodb-table"
   }
 }
